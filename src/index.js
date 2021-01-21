d94e9ba7fc545668cd5314c0eb5afcaf10a21f3c
佐野心哉 <shinya.s.0714@i.softbank.jp>
2021-01-21 13:05:09 +0900

    firebaseの追加処理

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import serviceWorker from "./serviceWorker";
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCb095O0kMltNMnL--McgZ_mJXiDeA1aO8",
+  authDomain: "todolist-3a584.firebaseapp.com",
+  projectId: "todolist-3a584",
+  storageBucket: "todolist-3a584.appspot.com",
+  messagingSenderId: "182827246096",
+  appId: "1:182827246096:web:975d8abcdc61b4a1e42095"
+};
+//firebaseの初期化
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <>
@@ -10,3 +24,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// serviceWorker.unregister();
